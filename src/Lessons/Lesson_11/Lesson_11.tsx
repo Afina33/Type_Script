23128cacb49fe56628b7fc113896146cd94ab843
ElenaGenergardt <P_L_Gen@web.de>
2025-12-17 08:56:31 +0100

    Add styles (elena)

--- src/Lessons/Lesson_11/Lesson_11.tsx
+++ src/Lessons/Lesson_11/Lesson_11.tsx
@@ -1,4 +1,46 @@
+import Input from "components/Input/Input";
+import {
+  PageWrapper,
+  Card,
+  Text,
+  ErrorText,
+  SearchContainer,
+  Container,
+  UniversitiesGrid,
+  UniversityCard,
+  UniversityName,
+  UniversityDetail,
+  } from "./styles";
+import Button from "components/Button/Button";
 
-export default function Lesson_11() {
-    
+function Homework_11() {
+  return (
+    <PageWrapper>
+      <Card>
+        <Text>University Search</Text>
+        <SearchContainer>
+          <Input
+            name={""}
+            label="Country"
+            placeholder="Enter Country for searching uneversities"
+            id={""}
+          />
+          <Button name={"Get Universities"}></Button>
+        </SearchContainer>
+        <Container>
+          <ErrorText></ErrorText>
+          <Text></Text>
+          <UniversitiesGrid>
+            <UniversityCard>
+              <UniversityName>имя института</UniversityName>
+              <UniversityDetail>данные</UniversityDetail>
+              <UniversityDetail>данные</UniversityDetail>
+              <UniversityDetail>данные</UniversityDetail>
+            </UniversityCard>
+          </UniversitiesGrid>
+        </Container>
+      </Card>
+    </PageWrapper>
+  );
 }
+export default Homework_11;
